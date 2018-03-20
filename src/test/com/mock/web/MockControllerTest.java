432466c1a5054f8d94da49cc6feb986bdf239112
qilong.chen <qilong.chen@qunar.com>
2018-03-20 13:50:38 +0800

    junit test

---
+++ src/test/com/mock/web/MockControllerTest.java
@@ -0,0 +1,45 @@
+package com.mock.web; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* MockController Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ÈýÔÂ 20, 2018</pre> 
+* @version 1.0 
+*/ 
+public class MockControllerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: execute(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "url", required = false) String url, @RequestParam(value = "expectation") String expectation) 
+* 
+*/ 
+@Test
+public void testExecute() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: insert(@RequestParam(value = "proto") String proto, @RequestParam(value = "domain") String domain, @RequestParam(value = "url") String url, @RequestParam(value = "expectation") String expectation) 
+* 
+*/ 
+@Test
+public void testInsert() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
