a9a74d94a560ded61fcd368e43c945d8a4df46a0
qilong.chen <qilong.chen@qunar.com>
2018-03-20 18:23:27 +0800

    pagecontroller

---
+++ src/main/java/com/mock/web/PageController.java
@@ -0,0 +1,22 @@
+package com.mock.web;
+
+import com.mock.service.MockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by qilong.chen on 2018/3/20.
+ */
+@Controller
+@RequestMapping(value = "/page")
+public class PageController {
+    @Autowired
+    private MockService mockService;
+
+    @RequestMapping(value = "mock")
+    public ModelAndView mockPage(){
+        return new ModelAndView("mock",null);
+    }
+}
