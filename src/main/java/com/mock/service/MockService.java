317c4822263327e40bb011b06914900422e318d9
qilong.chen <qilong.chen@qunar.com>
2018-03-20 16:18:27 +0800

    service

---
+++ src/main/java/com/mock/service/MockService.java
@@ -0,0 +1,19 @@
+package com.mock.service;
+
+import com.mock.pojo.MockInfo;
+
+import java.util.List;
+
+/**
+ * Created by qilong.chen on 2018/3/20.
+ */
+public interface MockService {
+
+    List<MockInfo> query();
+
+    void insert(MockInfo mockInfo);
+
+    void update(MockInfo mockInfo);
+
+    void delete(int id);
+}
---
+++ src/main/java/com/mock/service/impl/MockServiceImpl.java
@@ -0,0 +1,39 @@
+package com.mock.service.impl;
+
+import com.mock.dao.MockInfoDao;
+import com.mock.pojo.MockInfo;
+import com.mock.service.MockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by qilong.chen on 2018/3/20.
+ */
+@Service
+public class MockServiceImpl implements MockService {
+
+    @Autowired
+    private MockInfoDao mockInfoDao;
+
+    @Override
+    public List<MockInfo> query(){
+        return this.mockInfoDao.query();
+    }
+
+    @Override
+    public void insert(MockInfo mockInfo) {
+        this.mockInfoDao.insert(mockInfo);
+    }
+
+    @Override
+    public void update(MockInfo mockInfo){
+        this.mockInfoDao.update(mockInfo);
+    }
+
+    @Override
+    public void delete(int id){
+        this.mockInfoDao.delete(id);
+    }
+}
