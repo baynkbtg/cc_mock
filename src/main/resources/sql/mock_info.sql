a4dfa24fa53702ba89ffb3ae63435ffebbed61b9
qilong.chen <qilong.chen@qunar.com>
2018-04-04 18:53:15 +0800

    update

--- src/main/resources/sql/mock_info.sql
+++ src/main/resources/sql/mock_info.sql
@@ -1,5 +1,4 @@
 USE mock_server;
-drop table `mock_info`;
 CREATE TABLE `mock_info` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `alias` varchar(255) DEFAULT NULL COMMENT '接口名称',
@@ -7,5 +6,6 @@ CREATE TABLE `mock_info` (
   `domain` varchar(50) DEFAULT NULL COMMENT '域名',
   `path` varchar(255) DEFAULT NULL COMMENT '路径',
   `json` longtext COMMENT '期望值',
+  `iden` varchar(50) DEFAULT NULL COMMENT '标识',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
